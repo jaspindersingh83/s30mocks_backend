d1de934b6d76518c8a14bcb66361799f3a77531d
jaspindersingh83 <jaspindersingh83@gmail.com>
2025-04-13 22:17:23 +0530

    Timezone specific slots

--- models/InterviewSlot.js
+++ models/InterviewSlot.js
@@ -1,61 +1,65 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const InterviewSlotSchema = new mongoose.Schema({
   interviewer: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+    ref: "User",
+    required: true,
   },
   startTime: {
     type: Date,
-    required: true
+    required: true,
   },
   endTime: {
     type: Date,
-    required: true
+    required: true,
   },
   interviewType: {
     type: String,
-    enum: ['DSA', 'System Design'],
-    required: true
+    enum: ["DSA", "System Design"],
+    required: true,
   },
   isBooked: {
     type: Boolean,
-    default: false
+    default: false,
   },
   interview: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Interview'
+    ref: "Interview",
   },
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+    ref: "User",
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 // Validate slot duration based on interview type
-InterviewSlotSchema.pre('save', function(next) {
+InterviewSlotSchema.pre("save", function (next) {
   const duration = (this.endTime - this.startTime) / (1000 * 60); // duration in minutes
-  
-  if (this.interviewType === 'DSA') {
+
+  if (this.interviewType === "DSA") {
     // DSA interviews should be 40 minutes
-    if (Math.abs(duration - 40) > 1) { // Allow 1 minute tolerance
-      const error = new Error('DSA interview slots must be 40 minutes');
+    if (Math.abs(duration - 40) > 1) {
+      // Allow 1 minute tolerance
+      const error = new Error("DSA interview slots must be 40 minutes");
       return next(error);
     }
-  } else if (this.interviewType === 'System Design') {
+  } else if (this.interviewType === "System Design") {
     // System Design interviews should be 50 minutes
-    if (Math.abs(duration - 50) > 1) { // Allow 1 minute tolerance
-      const error = new Error('System Design interview slots must be 50 minutes');
+    if (Math.abs(duration - 50) > 1) {
+      // Allow 1 minute tolerance
+      const error = new Error(
+        "System Design interview slots must be 50 minutes"
+      );
       return next(error);
     }
   }
-  
+
   next();
 });
 
-module.exports = mongoose.model('InterviewSlot', InterviewSlotSchema);
+module.exports = mongoose.model("InterviewSlot", InterviewSlotSchema);
