285eda9b9065db61628655b1db04f1e1e08f5bfd
jaspindersingh83 <jaspindersingh83@gmail.com>
2025-04-13 14:35:03 +0530

    Adding Booking emails

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION || 'ap-south-1'
+  region: process.env.AWS_REGION || 'us-east-1'
 });
 
 // Create SES service object
